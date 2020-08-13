e6f2966d2f139519cb7d7b47a36150f24ce6414c
Austin Fouch <afouch2017@gmail.com>
2020-08-12 23:28:12 -0400

    Add files via upload

--- draft_to_excel.py
+++ draft_to_excel.py
@@ -1,38 +1,93 @@
-from sleeper_wrapper import User
 import requests
 import pandas as pd
-from pandas.io.json import json_normalize
-from openpyxl import load_workbook
+from openpyxl import Workbook, load_workbook
+'''
+TODO
+- create input for username
+- create input for year of draft
+- create input for new draft file
+'''
 
-with open ("./user.txt", "r") as hfile:
-  sp = hfile.read()
-line = sp.split("=")
-user = line[1]
 
-user = User(username)
-latest_draft_id = user.get_all_drafts("nfl", 2020)[0]['draft_id']
-URL = "https://api.sleeper.app/v1/draft/" + latest_draft_id + "/picks"
-latest_draft = requests.get(URL).json()
+'''
+function: get_all_user_drafts(username, year)
+parameters:
+  username - Sleeper.app username. Required for calls to Sleeper API for gathering draft data.
+  year - Year from which to pull draft(s) data. Required for calls to Sleeper API for gathering draft data.
+description:
+  Given a username and draft year, pull all draft data from Sleeper.app API for that user and year.
+  For each of the completed drafts of that year, create a diction
+returns:
+  List (drafts) of Lists (draft picks) of Dict (individual pick)
+'''
+def get_all_user_drafts(user, year):
+  
+  raw_user_drafts = requests.get(url="https://api.sleeper.app/v1/user/" + user['user_id'] + "/drafts/nfl/" + str(year))
+  raw_user_drafts = raw_user_drafts.json()
+  user_drafts = []
 
-# for the picks in the latest draft, grab those that belong to user and build their draft out
-my_picks = {}
-i = 0
-for pick in latest_draft:
-    if pick['picked_by'] == user.get_user_id():
-        my_pick = {}
-        my_pick['round'] = pick['round']
-        my_pick['pick_no'] = pick['pick_no']
-        my_pick['name'] = pick['metadata']['first_name'] + " " + pick['metadata']['last_name']
-        my_pick['team'] = pick['metadata']['team']
-        my_pick['position'] = pick['metadata']['position']
-        my_picks[i] = my_pick
-    i += 1
+  for draft in raw_user_drafts:
+    if draft['status'] == "complete":
+      
+      raw_curr_draft = requests.get(url="https://api.sleeper.app/v1/draft/" + draft['draft_id'] + "/picks")
+      curr_draft = raw_curr_draft.json()
+      
+      user_picks = []
+      for pick in curr_draft:
+        if pick['picked_by'] == user['user_id']:
+            
+            curr_user_pick = {}
 
-path = './sleeper_drafts.xlsx'
-book = load_workbook(path)
-writer = pd.ExcelWriter(path, enginer='openpyxl')
-writer.book = book
-df = pd.DataFrame(my_picks).transpose()
-df.to_excel(writer, index=False, sheet_name=latest_draft_id)
-writer.save()
-writer.close()
+            curr_user_pick['round'] = pick['round']
+            curr_user_pick['pick_no'] = pick['pick_no']
+            curr_user_pick['name'] = pick['metadata']['first_name'] + " " + pick['metadata']['last_name']
+            curr_user_pick['team'] = pick['metadata']['team']
+            curr_user_pick['position'] = pick['metadata']['position']
+
+            user_picks.append(curr_user_pick)
+      user_draft = {
+                    'draft_id' : draft['draft_id'],
+                    'draft_picks' : user_picks
+                  }
+      user_drafts.append(user_draft)
+  
+  return user_drafts
+
+def main():
+  
+  user = None
+  while user == None:
+    print('Enter your Sleeper.app username: ')
+    username = input()
+    req = requests.get(url="https://api.sleeper.app/v1/user/" + str(username))
+    user = req.json()
+    if user == None:
+      print("Invaid username!")
+  
+  path = './sleeper_drafts_2020.xlsx'
+  try:
+    book = load_workbook(path)
+  except FileNotFoundError:
+    print("File does not exist...creating new draft file...")
+    wb = Workbook()
+    wb.save(path)
+    book = load_workbook(path)
+  
+  writer = pd.ExcelWriter(path, engine='openpyxl')
+  writer.book = book
+
+  print("Gathering draft data...")
+  user_drafts = get_all_user_drafts(user, 2020)
+
+  print("Writing draft data to '" + path + "'...")
+  for draft in user_drafts:
+    if draft['draft_id'] not in book.sheetnames:
+        df = pd.DataFrame(draft['draft_picks'])
+        df = df[['pick_no','round', 'name', 'team', 'position']]
+        df.to_excel(writer, index=False, sheet_name=draft['draft_id'])
+        book[str(draft['draft_id'])].column_dimensions['c'].width = 30
+    
+  writer.save()
+  writer.close()
+  print("Complete")
+main()
